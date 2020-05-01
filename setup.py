6d7db3653116dd4d8774351abc4b49083a2fb5a0
Schmoderer <timothee.schmoderer@netcourrier.com>
2020-05-01 17:30:43 +0200

    update steup

--- setup.py
+++ setup.py
@@ -1,22 +1,29 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    README = fh.read()
 
 setuptools.setup(
     name="django-mdeditor-widget", # Replace with your own username
-    version="1.1.0",
+    version="1.1.3",
     author="Timothée Schmoderer",
     author_email="timothee.schmoderer@netcourrier.com",
     description="Django widget to edit markdown in forms",
-    long_description=long_description,
+    long_description=README,
     long_description_content_type="text/markdown",
+    license="GNU GPLv3",
     url="https://github.com/tschmoderer/django-mdeditor-widget",
-    packages=setuptools.find_packages(),
+    packages=['mdeditor', ],
     classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 3.0",  # Replace "X.Y" as appropriate
+        "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
     python_requires='>=3.6',
 )
